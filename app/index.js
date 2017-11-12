336ce9d37bf01a2bf0d0675164f8ae61993ff9b4
sxu2583 <shihabuddin100@gmail.com>
2017-11-11 23:21:10 -0500

    classes and inheritance

--- app/index.js
+++ app/index.js
@@ -1,6 +1,19 @@
 import mult from './math';
 import {fellowship, total} from './fellowship';
 
-console.log(mult(5,10));
 
-console.log(fellowship);
+class Animal{
+  constructor(name, height){
+    this.name = name;
+    this.height = height;
+  }
+
+  hello() {
+    console.log(`Hi! I'm ${this.name}`);
+  }
+}
+
+let king = new Animal("Mufasa", 4.5);
+
+console.log(king);
+king.hello();
