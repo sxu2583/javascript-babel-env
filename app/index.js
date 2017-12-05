c1af21d9bb1e23ee47411c030b1ef5ebf77677e1
sxu2583 <shihabuddin100@gmail.com>
2017-12-05 13:00:43 -0500

    Fixed bug not allowing react

--- app/index.js
+++ app/index.js
@@ -1,1 +1,7 @@
-console.log("Hello World");
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+ReactDom.render(
+  <div>React Applications!</div>, document.getElementById('root')
+);
--- build/bundle.js
+++ build/bundle.js
@@ -77,5 +77,21 @@ module.exports = __webpack_require__(1);
 "use strict";
 
 
+var _react = __webpack_require__(!(function webpackMissingModule() { var e = new Error("Cannot find module \"react\""); e.code = 'MODULE_NOT_FOUND'; throw e; }()));
+
+var _react2 = _interopRequireDefault(_react);
+
+var _reactDom = __webpack_require__(!(function webpackMissingModule() { var e = new Error("Cannot find module \"react-dom\""); e.code = 'MODULE_NOT_FOUND'; throw e; }()));
+
+var _reactDom2 = _interopRequireDefault(_reactDom);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+ReactDom.render(_react2.default.createElement(
+  'div',
+  null,
+  'React Applications!'
+), document.getElementById('root'));
+
 /***/ })
 /******/ ]);
